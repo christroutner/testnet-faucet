4cf633eed56c5a1b25fe2432b6e7ea1de8b352fc
Chris Troutner <chris.troutner@gmail.com>
2018-10-21 09:57:25 -0700

    Fixing docker networking

--- config/env/production.js
+++ config/env/production.js
@@ -3,6 +3,6 @@
 module.exports = {
   session: "secret-boilerplate-token",
   token: "secret-jwt-token",
-  database: "mongodb://localhost:3500/p2pvps-server-prod",
+  database: "mongodb://172.17.0.1:3500/p2pvps-server-prod",
   port: 3456
 }
