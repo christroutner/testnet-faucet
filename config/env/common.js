7783e80d3d21818a46e8638286aa0cd7a62077f4
Chris Troutner <chris.troutner@gmail.com>
2020-01-11 07:42:40 -0800

    Switching back to old tBCH address

--- config/env/common.js
+++ config/env/common.js
@@ -5,9 +5,9 @@
 // }
 
 module.exports = {
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || 3000,
   // Address of the app
-  addr: `bchtest:qz5zukfwts0afr83hh59tv2pkt0m480elcnthhy78e`,
+  addr: `bchtest:qqmd9unmhkpx4pkmr6fkrr8rm6y77vckjvqe8aey35`,
 
   // 0.1 BCH, represent in satoshis
   bchToSend: 10000000,
